d6bc1050975a53d800fa8513e3e160302eb7c509
Benjamin Marwell <bmarwell@apache.org>
2026-07-31 11:02:32 +0200

    (build) add a jreleaesr archive distribution

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,6 @@ module io.github.bmarwell.betterbranch {
     exports io.github.bmarwell.betterbranch;
     exports io.github.bmarwell.betterbranch.value;
     exports io.github.bmarwell.betterbranch.mapper;
+
+    requires transitive org.slf4j;
 }
